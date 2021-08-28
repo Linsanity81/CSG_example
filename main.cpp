e7610a0b5821199a2591fe2178dc3f861ecfedf2
Ziqi Wang <qiqiustc@gmail.com>
2021-08-28 11:22:43 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,34 +1,26 @@
 #include <igl/opengl/glfw/Viewer.h>
-
+#include <igl/readOBJ.h>
+#include <igl/copyleft/cgal/mesh_boolean.h>
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+    std::string mesh_obj_str = "/Users/ziqwang/Documents/GitHub/CSG_example/Bunny_12x12x9_K12_Strict_1_T0.02/Bunny_12x12x9.obj";
+    std::string puzzle_piece_str = "/Users/ziqwang/Documents/GitHub/CSG_example/Bunny_12x12x9_K12_Strict_1_T0.02/grid1_piece1.obj";
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
-  viewer.launch();
+    //read OBJ
+    Eigen::MatrixXd V0, V1, V2;
+    Eigen::MatrixXi F0, F1, F2;
+
+    igl::readOBJ(mesh_obj_str, V0, F0);
+    igl::readOBJ(puzzle_piece_str, V1, F1);
+
+    Eigen::VectorXi J;
+    igl::copyleft::cgal::mesh_boolean(V0, F0, V1, F1, igl::MeshBooleanType::MESH_BOOLEAN_TYPE_INTERSECT, V2, F2, J);
+
+    std::cout << V2 << std::endl;
+
+    // Plot the mesh
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V2, F2);
+    viewer.data().set_face_based(true);
+    viewer.launch();
 }
